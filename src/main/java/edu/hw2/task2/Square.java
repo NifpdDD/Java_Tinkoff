6e212ec307ced22587fb49954266182a21f20e6b
Pavel <naidanov_p@mail.ru>
2023-10-17 17:35:27 +0500

    hw2

--- src/main/java/edu/hw2/task2/Square.java
+++ src/main/java/edu/hw2/task2/Square.java
@@ -3,8 +3,6 @@ package edu.hw2.task2;
 public class Square extends Rectangle {
 
     public final Rectangle setSize(int size) {
-        super.setHeight(size);
-        super.setWidth(size);
         return new Rectangle(size,size);
     }
 }
