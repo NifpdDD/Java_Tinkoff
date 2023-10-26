748834bb2f0661d682e3ad4dbcea7f45bc742caa
Pavel <naidanov_p@mail.ru>
2023-10-26 13:40:38 +0500

    hw3

---
+++ src/main/java/edu/pr2/Coordinate.java
@@ -0,0 +1,4 @@
+package edu.pr2;
+
+public record Coordinate(int row, int col) {
+}
---
+++ src/main/java/edu/pr2/EulerGenerator.java
@@ -0,0 +1,8 @@
+package edu.pr2;
+
+public class EulerGenerator implements Generator{
+    @Override
+    public Maze generate(int height, int width) {
+
+    }
+}
