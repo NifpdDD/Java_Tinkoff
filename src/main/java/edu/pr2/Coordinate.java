817605b73462fa2f8b64aa182145a70df632457b
Pavel <naidanov_p@mail.ru>
2023-10-31 17:50:22 +0500

    hw3

--- src/main/java/edu/pr2/Coordinate.java
+++ src/main/java/edu/pr2/Coordinate.java
@@ -1,4 +1,19 @@
 package edu.pr2;
 
-public record Coordinate(int row, int col) {}
+import java.util.Objects;
+
+public record Coordinate(int row, int col) {
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Coordinate)) {
+            return false;
+        }
+        Coordinate other = (Coordinate) obj;
+        return Objects.equals(row, other.row) && Objects.equals(col, other.col);
+    }
+
+}
 
