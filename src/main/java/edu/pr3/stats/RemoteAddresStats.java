b5528c65b8cd10502bbb4d1b6527309b1d319158
Pavel <naidanov_p@mail.ru>
2023-11-20 00:38:34 +0500

    hw6

--- src/main/java/edu/pr3/stats/RemoteAddresStats.java
+++ src/main/java/edu/pr3/stats/RemoteAddresStats.java
@@ -1,22 +1,23 @@
 package edu.pr3.stats;
 
-import lombok.Getter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.Getter;
 
-public class RemoteAddres implements Stats{
+public class RemoteAddresStats implements Stats {
 
     public static final String TITLE = "Удаленные адреса";
     public static final List<String> HEADERS = List.of("Адрес", "Количество");
-    @Getter private static Map<String, Long> freqOfResources = new HashMap<>();
+    @Getter private static Map<String, Long> freqOfAddress = new HashMap<>();
 
     public static void addAdrress(String resources) {
-        RemoteAddres.freqOfResources.put(resources, RemoteAddres.freqOfResources.getOrDefault(resources, 0L) + 1);
+        RemoteAddresStats.freqOfAddress.put(resources, RemoteAddresStats.freqOfAddress.getOrDefault(resources, 0L) + 1);
     }
+
     public List<List<String>> getStats(int k) {
-        return RemoteAddres.freqOfResources.entrySet().stream()
+        return RemoteAddresStats.freqOfAddress.entrySet().stream()
             .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
             .limit(k)
             .map(entry -> Arrays.asList(entry.getKey(), entry.getValue().toString()))
