366d9aef173f5396b8616b490a36f4319332c132
Pavel <naidanov_p@mail.ru>
2023-11-20 17:50:24 +0500

    hw6

--- src/main/java/edu/pr3/Log.java
+++
@@ -1,7 +0,0 @@
-package edu.pr3;
-
-import java.time.OffsetDateTime;
-
-public record Log(String remoteAddr, String remoteUser, OffsetDateTime dateTimeLocal, String request,
-                  int status, long bodyBytesSent, String httpReferer, String httpUserAgent) {
-}
