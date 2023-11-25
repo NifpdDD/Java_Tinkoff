7083dc36fcafdfdd4a48a5499f3ea58ffe18aea2
Your Name <you@example.com>
2023-11-25 14:26:25 +0500

    Add Task2 (no test)

--- src/main/java/edu/hw7/task1/SingleInc.java
+++ src/main/java/edu/hw7/task1/SingleInc.java
@@ -1,4 +1,4 @@
-package edu.hw7;
+package edu.hw7.task1;
 
 import lombok.Getter;
 
---
+++ src/main/java/edu/hw7/task2/MultiFactorial.java
@@ -0,0 +1,22 @@
+package edu.hw7.task2;
+
+import java.util.stream.LongStream;
+
+public class MultiFactorial {
+    private Long value;
+
+    public MultiFactorial(Long value) {
+        checkValue(value);
+        this.value = value;
+    }
+
+    private static void checkValue(Long value) {
+        if (value <= 0) {
+            throw new IllegalArgumentException("value must be positive");
+        }
+    }
+
+    public Long countFactorial() {
+        return LongStream.range(0, value+1).parallel().reduce(1L, (a, b) -> a * b);
+    }
+}
