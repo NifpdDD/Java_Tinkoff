8b05265bb9b54202917c2f4144880b47fc6edc88
Your Name <you@example.com>
2023-12-12 02:04:12 +0500

    Revert "fix"
    This reverts commit b8f067358923e512a02079b26048275db81973b5.

--- src/main/java/edu/pr4/render/Render.java
+++
@@ -1,9 +0,0 @@
-package edu.pr4.render;
-
-import edu.pr4.Pixel;
-import edu.pr4.tranformation.Transformation;
-import java.util.List;
-
-public interface Render {
-    Pixel[][] render(List<Transformation> transformationList);
-}
